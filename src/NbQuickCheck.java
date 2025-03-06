43fb4a6e5f5c6a3c5d9408546779ac554392aea3
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-03-05 19:39:18 -0800

    Finished nbtree self-check

--- src/NbQuickCheck.java
+++ src/NbQuickCheck.java
@@ -14,6 +14,12 @@ public class NbQuickCheck {
     if(!tree.containsKey(root)) {
       return;
     }
+
+    System.out.println(root);
+
+    for (var child : tree.get(root)) {
+      preOrder(tree, child);
+    }
   }
 
   /**
@@ -24,7 +30,19 @@ public class NbQuickCheck {
    * @return the minimum value in the tree or Integer.MAX_VALUE if root is null
    */
   public static int minVal(Node<Integer> root) {
-    return -1;
+    if (root == null) {
+      return Integer.MAX_VALUE;
+    }
+
+    int min = root.value;
+
+    for (var child : root.children) {
+      int minChild = minVal(child);
+      if (minChild < min) {
+        min = minChild;
+      }
+    }
+    return min;
   }
   
 }
